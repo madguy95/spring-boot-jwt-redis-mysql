8e13ded95e13417d5a53d5941f2e4fb338d27b4a
madguy95 <nhoccon2111@gmail.com>
2023-04-16 20:25:02 +0700

    ok with @transactional

--- src/main/java/com/springjwt/security/services/UserDetailsServiceImpl.java
+++ src/main/java/com/springjwt/security/services/UserDetailsServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.springjwt.models.User;
 import com.springjwt.repository.UserRepository;
@@ -16,6 +17,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	private UserRepository userRepository;
 
 	@Override
+	@Transactional
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User user = userRepository.findByUsername(username)
 				.orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
